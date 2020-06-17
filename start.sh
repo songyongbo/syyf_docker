71f7bc31390490cadc445599d528ffe4bd0068f6
songyongbo <59037560@qq.com>
2020-06-17 16:17:36 +0800

    add back run

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ echo 'begin run ...=========================='
 chmod +x /go/micro
 chmod +x /go/account-srv
 chmod +x /go/console-api
-/go/micro gateway --handler=api --namespace=myj.rmsplus.api
-/go/account-srv
-/go/console-api
+/go/micro gateway --handler=api --namespace=myj.rmsplus.api &
+/go/account-srv &
+/go/console-api &
 
 while :
 do
