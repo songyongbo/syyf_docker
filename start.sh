1c5d643742ea06b4af248df1ff3388f7a41f55b7
songyongbo <59037560@qq.com>
2020-06-17 15:37:28 +0800

    增加其他启动文件

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # 还可以设置环境变量，读取JAVA_OPT 设置jvm参数
-echo 'begin run ...==================='
+echo 'begin run ...=========================='
 chmod +x /go/micro
+chmod +x /go/account-srv
+chmod +x /go/console-api
 /go/micro gateway --handler=api --namespace=myj.rmsplus.api
+/go/account-srv
+/go/console-api
 
 while :
 do
